0aaa02ccd6b2194ec489fda432b086012e52b812
Roan Chipman <rschipman@gmail.com>
2020-05-04 00:09:40 -0600

    second commit

--- adventure_game.py
+++ adventure_game.py
@@ -4,7 +4,7 @@ import random
 holster = []
 win = []
 enemy_list = ["murderous vampire", "wretched pirate Red Beard", "wicked witch", "evil fairie", "awful gym teacher" ]
-weapon_list = ["the Sword of Destiny", "the magic wand of Elderberry", "the mirror of truth", "a fart gun"]
+weapon_list = ["the Sword of Destiny", "the magic Wand of Elderberry", "the Mirror of Truth", "a fart gun"]
 weapon_choice = random.choice(weapon_list)
 enemy_choice = random.choice(enemy_list)
 
@@ -35,16 +35,17 @@ def restart():
     elif response == "2":
         the_cave()
 
-
 def intro():
     print_pause("You are standing in a field lush with wildflowers.")
     print_pause("You spot your trusty, yet rusted, short sword on "
     "the ground next to you and holster it. ")
     print_pause("You look around as you remember the cries of the "
-     "tortured townfolk you have been sent to save from a vicious, unknown enemy.")
+     "tortured townfolk you have been sent to save from a vicious, \n" 
+     "unknown enemy.")
     print_pause("Ahead of you, some distance away, \n"
      "lays a house shrouded in mystery . . . ")
-    print_pause("To your right a dark gloomy cave." ) 
+    print_pause("To your right a dark gloomy cave." )
+    the_field()
 
 
 def the_field():
@@ -80,13 +81,17 @@ def the_field_cave():
     print_pause("You return to the field")
     print_pause("To the right lays the mysterious house,")
     print_pause("to your back the cave")
-    response = valid_input("Would you like to proceed to the house?\n"
-     "Press '1' to proceed to the house '2' to return to you village with your new treasure \n", "1", "2")
-    if response == "1":
-        print_pause("You continue to the house.")
-        the_house()
-    elif response == "2":
+    if weapon_choice in holster:
+        response = valid_input("Would you like to proceed to the house?\n"
+        "Press '1' to proceed to the house '2' to return to you village with your new treasure \n", "1", "2")
+        if response == "1":
+            print_pause("You continue to the house.")
+            the_house()
+        elif response == "2":
+            the_village()
+    else:
         the_village()
+    
 
 def the_house():
     print_pause("The house appears empty")
@@ -103,10 +108,13 @@ def the_house():
             print_pause("The %s glares daggers in your heart before "
             "fleeing in fear" % enemy_choice)
             win.append("victory")
+            print_pause("You victoriously gather treasures scattered around the house " 
+            "to bring home to your village.")
+            the_village()
         else:
             print_pause("The noise nearly cripples you. "
             "You stagger and weakly draw your sword")
-            print_pause("Before you have a chance to strike you feel something forefully strike your head")
+            print_pause("Before you have a chance to strike you feel something forcefully strike your head")
             print_pause(". . . ")
             print_pause("You slowly awaken.")
             print_pause("You rise to your feet.")
@@ -117,33 +125,44 @@ def the_house():
 
 def the_village():
     if "victory" in win:
-        print_pause("The village rejoices as you ride back in to your home, face beaming with your victory")
+        print_pause("The villagers rejoice as you enter in to your home's village square, " 
+        "face beaming with your victory")
+        print_pause("The people lift you above their heads chanting your name in their victory cries")
         print_pause("YOU WIN!")
         exit()
     else:
         if weapon_choice in holster:
-            print_pause("The villagers throw old rottedd food at the greedy bastard they once believed to be a hero")
-            print_pause("Your greed has caused you to completly forget you mission")
+            print_pause("The villagers throw old rotted food at the greedy bastard they once believed to be a hero")
+            print_pause("Your greed has caused you to completly forget your mission")
             print_pause("The village will never forgive you.")
-            print_pause("+YOU LOSE+")
-def play_game():
+        else:
+            print_pause("You enter the village, your head hung low.")
+            print_pause("The villagers wont even look at the coward that returned without a fight")
+            print_pause("Ashamed you vow to never leave your house again")
+        you_lose()
+def start_game():
     print_pause("You a lowly farmer working the fields are approached by a frail, sickly looking, old man.")
     print_pause("He speaks quielty:")
     response = valid_input("'Are you a brave soul in search of a dangerous quest?' \n"
     "Please press '1' for Yes or '2' for No \n", "1", "2")
     if response == "1":
-        print_pause("'Thank goodness!'")
-        print_pause("'You are our saviour from this awful beast!'")
-        print_pause("'You shall leave immediatley!'")
-        print_pause("The man strikes you suddenly")
-        print_pause("You fall unconcious.")
-        print_pause("You awaken and arise slowly.")
-        print_pause(". . . ")
-        intro()
-        the_field()
+        play_game()
     elif response == "2":
         print_pause("Understood.")
         print_pause("So long.")
         exit()
 
-play_game()
+def you_lose():
+    print_pause("YOU LOSE")
+    exit()
+
+def play_game():
+    print_pause("'Thank goodness!'")
+    print_pause("'You are our saviour from this awful beast!'")
+    print_pause("'You shall leave immediatley!'")
+    print_pause("The man strikes you suddenly")
+    print_pause("You fall unconcious.")
+    print_pause("You awaken and arise slowly.")
+    print_pause(". . . ")
+    intro()
+
